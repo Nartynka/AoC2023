5d3b68d3779cdfa8479638bccdc1e066845a6486
Nartyna <mplutwska@gmail.com>
2023-12-01 11:25:36 +0100

    [Day 1] Second Star

--- Day 1/Day_1.py
+++ Day 1/Day_1.py
@@ -2,11 +2,35 @@ import re
 
 sum = 0
 
+dict = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'zero': '0'
+}
+
 with open("input.txt") as file:
     for line in file:
-        line = line.strip()
-        number1 = re.search("\d", line)[0]
-        number2 = re.findall("\d", line)[-1]
-        sum += int(number1 + number2)
+      line = line.strip()
+      num_str1 = re.search(r"(\d|one|two|three|four|five|six|seven|eight|nine)", line)[0]
+      num_str2 = re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line)[-1]
+
+      number = ""
+      if dict.get(num_str1):
+         number = dict.get(num_str1)
+      else:
+         number = num_str1
+         
+      if dict.get(num_str2):
+         number += dict.get(num_str2)
+      else:
+         number += num_str2
+      sum += int(number)
 
 print(sum)
